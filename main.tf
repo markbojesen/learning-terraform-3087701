45a473ffe9b024fcdf6cde11f040f6face81cc2d
Mark Bojesen <markbojesen@users.noreply.github.com>
2024-07-11 09:24:34 +0100

    fixed typo and get rid of old security group

--- main.tf
+++ main.tf
@@ -29,13 +29,6 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
@@ -43,7 +36,7 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
 
-  ingress _rules      = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules      = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules      = ["all-all"]
